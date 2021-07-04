1f18f17ea49d22db5952fdc398349cad32076f1b
VimuthM <vimuthmendis@gmail.com>
2021-07-05 01:47:16 +0800

    Add production settings

---
+++ chatb/settings/production.py
@@ -0,0 +1,17 @@
+import os # noqa
+
+from pymongo import MongoClient # noqa
+
+from chatb.settings.base import *  # noqa
+
+DEBUG = False
+
+ALLOWED_HOSTS.append("eusoffchatbot.herokuapp.com")
+
+mongodb_user = os.getenv("MONGO_USER")
+mongodb_password = os.getenv("MONGO_PASSWORD")
+mongodb_host = os.getenv("MONGO_HOST")
+MONGO_CLIENT = MongoClient(
+    f"mongodb+srv://{mongodb_user}:{mongodb_password}@{mongodb_host}"
+)
+MONGO_DB = MONGO_CLIENT.chatb
