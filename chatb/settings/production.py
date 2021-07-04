dd5c4bb094d63f87917f375ed6ec4faa954081b4
VimuthM <vimuthmendis@gmail.com>
2021-07-05 03:16:05 +0800

    Change mongo url

--- chatb/settings/production.py
+++ chatb/settings/production.py
@@ -12,6 +12,6 @@ mongodb_user = os.getenv("MONGO_USER")
 mongodb_password = os.getenv("MONGO_PASSWORD")
 mongodb_host = os.getenv("MONGO_HOST")
 MONGO_CLIENT = MongoClient(
-    f"mongodb://{mongodb_user}:{mongodb_password}@{mongodb_host}"
+    f"mongodb+srv://{mongodb_user}:{mongodb_password}@{mongodb_host}"
 )
 MONGO_DB = MONGO_CLIENT.chatb
