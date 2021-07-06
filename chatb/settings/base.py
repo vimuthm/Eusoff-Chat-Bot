47959520a66ffe55e5905ddc88dc1ce295e38868
Jared <jlhfqaz@gmail.com>
2021-07-06 18:45:23 +0800

    revert to original

--- chatb/settings/base.py
+++ chatb/settings/base.py
@@ -128,4 +128,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "static_files")
+STATIC_ROOT = BASE_DIR / "static_files"
