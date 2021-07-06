aa054a79ceb1b8fedd387d6ef67ce202a81d26f6
Jared <jlhfqaz@gmail.com>
2021-07-06 17:47:37 +0800

    changed heroku web link

--- chatb/settings/base.py
+++ chatb/settings/base.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 from pymongo import MongoClient
@@ -127,4 +128,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = BASE_DIR / "static_files"
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "static_files")
