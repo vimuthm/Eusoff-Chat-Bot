cde5079d330f62cf2fe6050d3bc1f45a96bf1a99
VimuthM <vimuthmendis@gmail.com>
2021-07-09 03:52:38 +0800

    debug

--- chatb/foo.py
+++ chatb/foo.py
@@ -4,11 +4,11 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 def aiChat(input):
     chatbot = ChatBot(
         'Herbert',
-        logic_adapters=[
-            'chatterbot.logic.UnitConversion',
-            'chatterbot.logic.MathematicalEvaluation',
-            'chatterbot.logic.TimeLogicAdapter'
-        ]
+        # logic_adapters=[
+        #     'chatterbot.logic.UnitConversion',
+        #     'chatterbot.logic.MathematicalEvaluation',
+        #     'chatterbot.logic.TimeLogicAdapter'
+        # ]
     )
 
     trainer = ChatterBotCorpusTrainer(chatbot)
@@ -18,3 +18,22 @@ def aiChat(input):
     trainer.train("chatterbot.corpus.english.conversations")
 
     return chatbot.get_response(input)
+
+print(aiChat("what are your thoughts on loki"))
+# from chatbot import Chat, register_call
+# import wikipedia
+
+# @register_call("whoIs")
+# def who_is(session, query):
+#     try:
+#         return wikipedia.summary(query)
+#     except Exception:
+#         for new_query in wikipedia.search(query):
+#             try:
+#                 return wikipedia.summary(new_query)
+#             except Exception:
+#                 pass
+#     return "I don't know about "+query
+
+# first_question="Hi, how are you?"
+# Chat("examples/Example.template").converse(first_question)
