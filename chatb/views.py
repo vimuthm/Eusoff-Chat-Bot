e530db43806f155f206df09c8585ef99144e31d7
Jared <jlhfqaz@gmail.com>
2021-07-08 11:31:10 +0800

    test

--- chatb/views.py
+++ chatb/views.py
@@ -65,10 +65,15 @@ class ChatBotView(View):
         keyboard = {
             "inline_keyboard": [
                 [
-                    {"text": "1", "callback_data": 1}
+                    {"text": "1", "callback_data": 1},
+                    {"text": "2", "callback_data": 2},
+                    {"text": "3", "callback_data": 3},
+                    {"text": "4", "callback_data": 4},
+                    {"text": "5", "callback_data": 5}
                 ]
             ]}
         self.send_message("test", t_id, reply_markup=keyboard)
+        print(self.send_message("test", t_id, reply_markup=keyboard))
 
         if not chat:
             if text != "register":
