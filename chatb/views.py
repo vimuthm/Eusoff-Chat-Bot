257196ddeb116a0cf1aa10411ea61425c5ccf727
Jared <jlhfqaz@gmail.com>
2021-07-12 19:03:59 +0800

    fix underscore in leaderboard message

--- chatb/views.py
+++ chatb/views.py
@@ -185,7 +185,7 @@ class ChatBotView(View):
                 for doc in cursor:
                     print(doc)
                     msg += "%d. Tele: %s \n Matches: %d \n Rating: %d \n" % (
-                        count, doc["first_name"], doc["count"], doc["rating"])
+                        count, doc["tele"], doc["count"], doc["rating"])
                     count += 1
                 msg.replace("_", "\\_")
                 self.send_message(msg, t_id)
